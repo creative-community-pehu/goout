1be7d38d2bfe934b562ef550bb690fea466d4d0f
∧° ┐ <we.are.pe.hu@gmail.com>
2021-10-12 20:07:52 +0900

    members

---
+++ members/relax/hotel/index.php
@@ -0,0 +1,161 @@
+<?php
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$name = (string)filter_input(INPUT_POST, 'name');
+$address = (string)filter_input(INPUT_POST, 'address');
+$tag = (string)filter_input(INPUT_POST, 'tag');
+$info = (string)filter_input(INPUT_POST, 'info');
+$link = (string)filter_input(INPUT_POST, 'link');
+
+$fp = fopen('list.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$name, $address, $tag, $info, $link,]);
+    rewind($fp);
+}
+
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title> Fancy Hotel Tour | Fancy and Relaxin' </title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="http://creative-community.pe.hu/coding/js/org.js"></script>
+<script src="http://creative-community.pe.hu/coding/js/smoothscroll.js"></script>
+<script type="text/javascript">
+$(function(){
+})
+</script>
+<link rel="stylesheet" href="http://creative-community.pe.hu/coding/submit/org/book.css"/>
+<link rel="stylesheet" href="http://creative-community.pe.hu/coding/css/radius.css"/>
+<link rel="stylesheet" href="http://goout.pe.hu/map/style.css" />
+<link rel="stylesheet" href="http://goout.pe.hu/style.css" />
+<link rel="stylesheet" href="http://goout.pe.hu/about.css" />
+<link rel="stylesheet" href="/goout/style.css" />
+<style type="text/css">
+
+body {
+  background-color: #ebd809;
+}
+#org .tag .label {font-family: "DiaPro";}
+#org .address .label {
+  background-color:lemonchiffon;
+  color: lightskyblue;
+}
+#org .address .label:hover {
+  background-color:lightskyblue;
+  color: lemonchiffon;
+}
+.address {zoom:0.5;}
+.list li span {
+  animation:2s ease-in infinite fontmotion;
+}
+
+    .cover {
+      font-family: "Yu Gothic UI";
+      background:#111;
+    }
+    .cover span {
+      color:#fff;
+      zoom: 200%;
+    }
+    .cover h1 {
+      text-align: center;
+      text-shadow:0 0 0.5vw lemonchiffon;
+      color:#ebd809;
+      font-family:"DiaPro";
+      font-size: 12.5vw;
+    }
+    .cover h1 i {
+      color:#000;
+      background-color: #ebd809;
+      display:inline-block;
+      font-family:"DiaPro";
+      font-size: 2.5vw;
+      padding:0 1vw;
+    }
+    .cover a {color:#000;}
+
+    .cover {
+      background-image:url();
+      background-color: #000;
+    }
+    #org h1 {
+      display: inline-block;
+      padding: 0 0.25rem;
+      color:#ebd809;
+      font-size: 1rem;
+      background-color: #000;
+      font-family: "ipaexg";
+    }
+
+      .relax #center a {
+        text-align:center;
+        animation:2s linear infinite alternate relax;
+      }
+      .relax #center a:hover {
+        cursor:pointer;
+        animation:.25s linear infinite alternate relax;
+      }
+
+      @keyframes relax {
+        0% {
+          color:#ebd809;
+          background:#000;
+        }
+        100% {
+          color:#000;
+          background:#ebd809;
+        }
+        @-webkit-keyframes relax {
+          0% {
+          color:#ebd809;
+          background:#000;
+          }
+          100% {
+          color:#000;
+          background:#ebd809;
+          }
+        }
+      }
+
+</style>
+</head>
+<body>
+<div class="cover">
+  <span class="left goout">Go Out</span>
+  <span class="right relax">Presents</span>
+  <h1>Fancy Hotel Tour</h1>
+</div>
+
+<ul class="list">
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<li id="<?=h($row[2])?>" class="list_item list_toggle radius" data-address="<?=h($row[1])?>" data-tag="<?=h($row[2])?>">
+<p><?=h($row[3])?></p>
+<span><?=h($row[0])?></span>
+<a href="<?=h($row[4])?>"></a>
+</li>
+<?php endforeach; ?>
+<?php else: ?>
+<li id="<?=h($row[2])?>" class="list_item list_toggle radius" data-address="<?=h($row[1])?>" data-tag="<?=h($row[2])?>">
+<p>Name</p>
+<span>Title</span>
+<a style="display:none;" href="none" target="_blank" rel="noopener noreferrer"></a>
+</li>
+<?php endif; ?>
+</ul>
+<hr/>
+
+</body>
+</html>
---
+++ members/relax/index.php
@@ -0,0 +1,163 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width">
+<style type="text/css">
+
+#date_list li {
+  display:none;
+}
+#btn-play{
+  position:relative;
+  z-index:10;
+  display:block;
+  width:75%;
+  margin:0 auto 2.5rem;
+  border:none;
+  font-family: "DiaPro";
+  font-size: 2rem;
+}
+.close {filter: invert(1);}
+
+.about #link {
+  position:absolute;
+	cursor:pointer;
+  margin:0;
+  top:0; left:0;
+  width:100%; height:100%;
+  text-indent:-999px;
+}
+.about:hover {
+  cursor:pointer;
+  color:#fff;
+  background:#eee;
+  text-shadow:0.1rem 0.1rem #000;
+  transition:.5s all;
+}
+.relax a {
+  animation:2s linear infinite alternate relax;
+}
+.relax a:hover {
+  animation:0.25s linear infinite alternate relax;
+}
+
+@-webkit-keyframes relax {
+  0% {
+    color:#ebd809;
+    background:#000;
+  }
+  100% {
+    color:#000;
+    background:#ebd809;
+  }
+}
+
+@media screen and (max-width: 750px){
+}
+
+.ipaexg {font-family: "ipaexg";}
+.DiaPro, .sub {font-family: "DiaPro";}
+
+@media screen and (max-width: 750px){
+#date_list li {
+  width:100%;
+}
+}
+
+
+.print{display:none;}
+
+@media print{
+.print{display:block;}
+.no_print{display: none;}
+}
+</style>
+<title>Fancy and Relaxin' | Go Out</title>
+<link rel="icon" href="logo.png">
+<link rel="stylesheet" type="text/css" href="styles.css" />
+<link rel="stylesheet" type="text/css" href="../review.css" />
+<link rel="stylesheet" href="https://creative-community.space/coding/fontbook/css/font-family.css" />
+</head>
+<body>
+<div class="popup" id="submit" style="display:none;">
+<p><iframe src="submit.php"></iframe></p>
+<span class="close" onclick="obj=document.getElementById('submit').style; obj.display=(obj.display=='none')?'block':'none';">✕</span>
+</div>
+<div class="about">
+<div class="inside">
+<span class="sub"><b>Fancy and Relaxin'</b></span>
+<span class="right DiaPro"><u class="no_print">投稿する | <i>Submit</i></u></span>
+<a id="link" class="tag" onclick="obj=document.getElementById('submit').style; obj.display=(obj.display=='none')?'block':'none';"></a>
+</div>
+</div>
+
+<div id="dance"></div>
+
+<div class="no_print">
+<audio id="bgm1" preload loop>
+  <source src="If_I_Were_A_Bell.mp3" type="audio/mp3">
+</audio>
+
+<button id="btn-play" type="button"><b class="fas fa-play">"We'll play and tell you what it is later"</b></button>
+</div>
+
+<div id="announce">
+<div class="greeting">
+<h1 class="DiaPro">行ったことのない場所へ行き、その場所を「贅沢度・リラックス度」を基準に評価し、優雅にリラックスできるスポットのコレクションを制作しています。</h1>
+</div>
+</div>
+
+<div id="dance"></div>
+<div id="info">
+<div class="greeting">
+<b>ファンシー・アンド・リラキシン</b>
+</div>
+<h1 class="DiaPro">行ったことのない場所へ（知らない道を選んで）、行きましょう。<br/>
+<span>最短ルートではなく、贅沢さ・心地よさを基準に、忙しくても時を忘れて、優雅にリラックスするために、知らない道を選んで行ったことのない場所へ行く。</span></h1>
+</div>
+<div id="announce">
+<div class="greeting">
+<p>ファンシー・アンド・リラキシンは、行ったことのない場所に行く／知らない道を選ぶことで、気分をリフレッシュし、こ・こ・ろ・豊かな生活を送ることを目指した活動です。</p>
+<p>スポットの紹介と同時に、行ったことのない場所に行く／知らない道を選ぶことの楽しさを、記録していきたいと思っています。</p>
+</div>
+</div>
+
+<div class="no_print">
+<div id="review"></div>
+<hr/>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="http://creative-community.space/coding/js/org.js"></script>
+<script type="text/javascript">
+$(function(){
+$("#review").load("/members/relax/review.php");
+$("#dance").load("/members/relax/image.html");
+})
+</script>
+<script>
+  const bgm1 = document.querySelector("#bgm1");       // <audio>
+  const btn  = document.querySelector("#btn-play");   // <button>
+
+  btn.addEventListener("click", ()=>{
+    // pausedがtrue=>停止, false=>再生中
+    if( ! bgm1.paused ){
+      btn.innerHTML = '<b class="fas fa-play">from Album "Relaxin with the Miles Davis Quintet"</b>';  // 「再生ボタン」に切り替え
+      bgm1.pause();
+    }
+    else{
+      btn.innerHTML = '<b class="fas fa-pause">BGM: If I Were A Bell by The Miles Davis Quintet</b>';  // 「一時停止ボタン」に切り替え
+      bgm1.play();
+    }
+  });
+
+  /**
+   * [event] 再生終了時に実行
+   */
+   bgm1.addEventListener("ended", ()=>{
+    bgm1.currentTime = 0;  // 再生位置を先頭に移動(こいつはなくても大丈夫です)
+    btn.innerHTML = '<b class="fas fa-play"></b>';  // 「再生ボタン」に変更
+  });
+</script>
+</body>
+</html>
