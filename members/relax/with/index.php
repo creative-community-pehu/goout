dc699fb8bdd030da363019f2921f4cfa11d65eb9
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-13 11:44:11 +0900

    Create index.php

---
+++ members/relax/with/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>Fancy and Relaxin' | with</title>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta name="description" content="行ったことのない場所へ（知らない道を選んで）、行く">
+<link rel="icon" href="/map/members/relax/logo.png">
+<link rel="stylesheet" href="/map/css/links.css"/>
+<link rel="stylesheet" href="/map/css/fonts.css"/>
+<link rel="stylesheet" href="https://creative-community.space/coding/fontbook/css/font-family.css"/>
+<style>
+body { margin: 0; padding: 0; }
+</style>
+</head>
+
+<body>
+<div class="click">
+<h1 class="left relax"><b>Fancy and</b></h1>
+<h1 class="right relax"><b>Relaxin' with</b></h1>
+<p class="btn center relax"><a href="/map/members/relax/" target="_parent">Members Only</a></p>
+</div>
+</body>
+</html>
