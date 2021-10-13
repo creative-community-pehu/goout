4ed35da0c60047b458e4627925f92a3d833318be
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-13 22:16:31 +0900

    Update index.php

--- update/index.php
+++ update/index.php
@@ -57,7 +57,7 @@ body { margin: 0; padding: 0; }
 <a href="<?=h($row[3])?>" target="_parent"></a></p>
 </li>
 <?php endif; ?>
-<h2 class="relax" style"text-align:center;"><b class="goout">Topics</b></h2>
+<h1 class="relax" style="text-align:center;"><b class="goout">Topics</b></h1>
 </ul>
 </body>
 </html>
