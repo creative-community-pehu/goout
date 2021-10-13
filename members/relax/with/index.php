dceb0b8e3be9bbc8e42242ff993dafd1e4eeb7a4
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-13 11:50:44 +0900

    Update index.php

--- members/relax/with/index.php
+++ members/relax/with/index.php
@@ -1,3 +1,29 @@
+
+<?php
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$format = (string)filter_input(INPUT_POST, 'format');
+$date = (string)filter_input(INPUT_POST, 'date');
+$comment = (string)filter_input(INPUT_POST, 'comment');
+$link = (string)filter_input(INPUT_POST, 'link');
+
+$fp = fopen('date.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$format, $date, $comment, $link]);
+    rewind($fp);
+}
+
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -17,10 +43,20 @@ body { margin: 0; padding: 0; }
 </head>
 
 <body>
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<div class="click">
+<h1 class="left relax"><b><?=h($row[0])?></b></h1>
+<h1 class="right relax"><b><?=h($row[1])?></b></h1>
+<p class="btn center relax"><a href="<?=h($row[3])?>" target="_parent"><?=h($row[2])?></a></p>
+</div>
+<?php endforeach; ?>
+<?php else: ?>
 <div class="click">
 <h1 class="left relax"><b>Fancy and</b></h1>
 <h1 class="right relax"><b>Relaxin' with</b></h1>
 <p class="btn center relax"><a href="/map/members/relax/" target="_parent">Members Only</a></p>
 </div>
+<?php endif; ?>
 </body>
 </html>
