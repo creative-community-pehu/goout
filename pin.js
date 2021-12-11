523101248625893fc3e6b1e3d96333d4a2a93ea1
pe-hu <we.are.pe.hu@gmail.com>
2021-12-11 16:39:14 +0900

    Update pin.js

--- pin.js
+++ pin.js
@@ -5,12 +5,10 @@ const geojson = {
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [34.69883713756516,135.49299331023352]
+        coordinates: [-77.032, 38.913]
       },
       properties: {
-        title: 'OSAKA GARDEN CITY',
-        iframe: 'https://www.google.com/maps/embed?pb=!4v1639207891102!6m8!1m7!1sCAoSLEFGMVFpcE80TlBvQmVBWV9mTlhSeGZGSWR4eEw5ckhuY1VyLXV6N0drN0x1!2m2!1d34.6986431!2d135.4924676!3f0!4f20!5f0.7820865974627469',
-        url: 'https://goo.gl/maps/JVotUK14EoaExpJD8'
+        title: 'Mapbox',
       }
     },
     {
