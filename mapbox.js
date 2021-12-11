8a73d779cd180601ca611c717f0861bbbaacc8bc
pe-hu <we.are.pe.hu@gmail.com>
2021-12-11 17:38:23 +0900

    Update mapbox.js

--- mapbox.js
+++ mapbox.js
@@ -5,10 +5,48 @@ const geojson = {
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [-77.032, 38.913]
+        coordinates: [135.45799310807644,34.63296956355602]
       },
       properties: {
-        title: 'Mapbox',
+        title: '船町渡船場',
+        iframe: 'https://www.youtube.com/embed/Z8Von0j_br8',
+        url: 'https://youtu.be/Z8Von0j_br8'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.46306770448456,34.6261385710099]
+      },
+      properties: {
+        title: '木津川渡船場',
+        iframe: 'https://www.youtube.com/embed/JDQjc70a1T8',
+        url: 'https://youtu.be/JDQjc70a1T8'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.47595609248816,34.632416979359874]
+      },
+      properties: {
+        title: '千本松渡船場',
+        iframe: 'https://www.youtube.com/embed/SNw7eE2K0c0',
+        url: 'https://youtu.be/SNw7eE2K0c0'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49249037915752,34.698777739416045]
+      },
+      properties: {
+        title: '阪急百貨店梅田店コンコース',
+        iframe: 'https://www.google.com/maps/embed?pb=!4v1639211054591!6m8!1m7!1sCAoSLEFGMVFpcFBaVEdndE85eUlyT0t4ZjMxWTJfbjhvZDgtekpTbERGM3R6dVJM!2m2!1d34.7028186!2d135.4985323!3f0!4f0!5f0.7820865974627469',
+        url: 'https://youtu.be/qzIy_qtpNh8'
       }
     },
     {
@@ -19,8 +57,8 @@ const geojson = {
       },
       properties: {
         title: 'OSAKA GARDEN CITY',
-        iframe: 'https://www.google.com/maps/embed?pb=!4v1639207891102!6m8!1m7!1sCAoSLEFGMVFpcE80TlBvQmVBWV9mTlhSeGZGSWR4eEw5ckhuY1VyLXV6N0drN0x1!2m2!1d34.6986431!2d135.4924676!3f0!4f20!5f0.7820865974627469',
-        url: 'https://goo.gl/maps/JVotUK14EoaExpJD8'
+        iframe: 'https://www.google.com/maps/embed?pb=!4v1639211015433!6m8!1m7!1sCAoSLEFGMVFpcFBPSE96OTgzaUVqREREXzZ1Rko0RU9uV1hNYzFzLVdMaDd5SU4z!2m2!1d34.6986431!2d135.4924676!3f0!4f0!5f0.7820865974627469',
+        url: 'https://verygois.bandcamp.com/album/garden-city'
       }
     }
   ]
