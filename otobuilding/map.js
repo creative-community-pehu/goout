a47dfd001c313b5d3de051cb9aa7261ae68386f0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-23 03:13:45 +0900

    otobuil

--- otobuilding/map.js
+++ otobuilding/map.js
@@ -27,6 +27,91 @@ const geojson = {
                 link: 'link',
                 url: 'https://newlifecollection.com/ca100/317/p-r-s/'
             }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47553890624397, 34.62278612421075]
+            },
+            properties: {
+                title: '楽しい。笑顔',
+                date: '2020年9月12日 21:00',
+                address: '住之江区北加賀屋５丁目３',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca100/315/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47455957175714, 34.63217273804081]
+            },
+            properties: {
+                title: '間も無く出港します。係員の指示に従って速やかにご乗船ください',
+                date: '2022年2月7日 18:10',
+                address: '大正区南恩加島１丁目１１−１',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca98/207/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4621538444374, 34.626643494172384]
+            },
+            properties: {
+                title: 'はいどうぞーあぃ！',
+                date: '2022年2月7日 16:00',
+                address: '大正区船町１丁目１−４',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca98/208/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.45803222026774, 34.63324675480992]
+            },
+            properties: {
+                title: 'えりゃありょ',
+                date: '2022年2月7日 16:05',
+                address: '大正区鶴町１丁目１６−６１',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca98/285/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46692081091408, 34.634831295508775]
+            },
+            properties: {
+                title: 'まーっすぐ！まーっすぐ！背中まーっすぐ！',
+                date: '2022年2月7日 16:28',
+                address: '大正区南恩加島６丁目７',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca98/206/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4783593740841, 34.62293030222976]
+            },
+            properties: {
+                title: 'やば、パジャマじゃ入れやんやん',
+                date: '2020年8月19日 13:00頃',
+                address: '住之江区北加賀屋２丁目５−３１',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca100/316/p-r-s/'
+            }
         }
+
     ]
 };
