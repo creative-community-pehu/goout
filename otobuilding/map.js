cf24958729cb32b1997913358920fe55181378e5
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-23 03:02:14 +0900

    otobuil

---
+++ otobuilding/map.js
@@ -0,0 +1,32 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47306292634534, 34.62458544610712]
+            },
+            properties: {
+                title: '春はなんかおかしいですわ',
+                date: '2022年3月25日 11:30',
+                address: '住之江区北加賀屋５丁目５−１',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca100/1277/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4738534037411, 34.62493209288219]
+            },
+            properties: {
+                title: '温めましょうね',
+                date: '2021年11月15日 16:40',
+                address: '住之江区北加賀屋５丁目４−７０',
+                link: 'link',
+                url: 'https://newlifecollection.com/ca100/317/p-r-s/'
+            }
+        }
+    ]
+};
