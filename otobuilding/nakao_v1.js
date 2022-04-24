e3fe4a279e5b4f41216d72ff2a1bc000818d1923
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-24 17:04:00 +0900

    nakao_v1.js

---
+++ otobuilding/nakao_v1.js
@@ -0,0 +1,229 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4875474720384, 34.63374890837616]
+            },
+            properties: {
+                title: 'やるぞ ‼︎',
+                date: '2021年12月5日 11:07',
+                address: '西成区千本中２丁目３ー１',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47856187287965, 34.62529785418573]
+            },
+            properties: {
+                title: 'バニラとチョコチップで ',
+                date: '2021年12月8日 19:00',
+                address: '住之江区北加賀屋３丁目１−２０',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47936383460274, 34.622953173247666]
+            },
+            properties: {
+                title: '振り返ってもらいま〜す',
+                date: '2022年2月10日 11:57',
+                address: '住之江区北加賀屋２丁目５−２６',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47663935011116, 34.6203679507164]
+            },
+            properties: {
+                title: 'ヤマガタさんが買ってきてくれるような煎餅やね',
+                date: '2022年2月10日 12:33',
+                address: '住之江区西加賀屋１丁目２−３４',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48303366711173, 34.62825847640973]
+            },
+            properties: {
+                title: 'わっけわからん事言いよって',
+                date: '2022年2月16日 12:18',
+                address: '西成区南津守６丁目１−７５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49030845757147, 34.629224873576504]
+            },
+            properties: {
+                title: '雪止んだねんけど',
+                date: '2022年2月16日 14:31',
+                address: '西成区千本南１丁目２１−２３−２５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4659611866325, 34.6207982219169]
+            },
+            properties: {
+                title: 'バリしんどかったっすよ',
+                date: '2022年2月18日 12:38',
+                address: '住之江区柴谷１丁目２−６７',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46283814364557, 34.608090149070875]
+            },
+            properties: {
+                title: '「渡るで」「渡るん？」「渡るやろ」',
+                date: '2022年2月18日 13:00',
+                address: '住之江区新北島７丁目４',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46698559062287, 34.6063998431698]
+            },
+            properties: {
+                title: 'テッテレー',
+                date: '2022年2月18日 13:48',
+                address: '住之江区新北島３丁目８−１２',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46531234843206, 34.60793427450585]
+            },
+            properties: {
+                title: '鬼滅鬼滅鬼滅欲しいー！',
+                date: '2022年2月18日 14:31',
+                address: '住之江区新北島５丁目２−１８',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4639189722228, 34.6077978947211]
+            },
+            properties: {
+                title: 'サラリーマンはなんぼ頑張ってもなぁ',
+                date: '2022年2月18日 14:51',
+                address: '住之江区新北島５丁目２−２２',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46527845549628, 34.606859411026804]
+            },
+            properties: {
+                title: 'いっせーのーでっ<br/>いっせーのーでっ',
+                date: '2022年2月18日 15:00',
+                address: '住之江区新北島３丁目８−３３',
+                link: 'none',
+                url: '135.46527845549628,34.606859411026804'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46833474746012, 34.603447361713776]
+            },
+            properties: {
+                title: '３段でいける〜？',
+                date: '2022年2月18日 15:22',
+                address: '住之江区新北島２丁目８',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47899511572933, 34.603156712046015]
+            },
+            properties: {
+                title: '4時くらいちゃいますの',
+                date: '2022年2月18日 15:39',
+                address: '住之江区北島２丁目８−５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4902910745509, 34.623562311613156]
+            },
+            properties: {
+                title: '銀行って大きいじゃないですか',
+                date: '2022年2月25日 15:51',
+                address: '西成区玉出西２丁目６',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47402415631063, 34.613069404243646]
+            },
+            properties: {
+                title: '末は社長や',
+                date: '2022年4月10日 12:00',
+                address: '住之江区南加賀屋１丁目１−１１７',
+                link: 'none',
+                url: '関連記事'
+            }
+        }
+
+    ]
+};
