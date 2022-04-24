129e62f43edc5cb58ecf007cc169373d7b77bee8
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-24 23:37:35 +0900

    aa

---
+++ otobuilding/aa_v1.js
@@ -0,0 +1,89 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47290133878147, 34.60749330611684]
+            },
+            properties: {
+                title: '偉かったね、もう終わったよ。よくがんばったね〜',
+                date: '2022年3月17日 16:50',
+                address: '住之江区新北島１丁目２−１',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48059970640924, 34.612530112983364]
+            },
+            properties: {
+                title: 'ドキドキするけど大丈夫！',
+                date: '2022年2月8日 15:30',
+                address: '住之江区浜口東１丁目１',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4827294191162, 34.60191716499787]
+            },
+            properties: {
+                title: 'なぁ、ほんまのこと言うて？',
+                date: '2022年2月8日 16:25',
+                address: '住之江区西住之江３丁目３',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46463988902042, 34.6063333988492]
+            },
+            properties: {
+                title: 'でっか！なんやねんこの白菜',
+                date: '2022年2月8日 18:50',
+                address: '住之江区新北島６丁目１−５５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4808284868539, 34.61827594571754]
+            },
+            properties: {
+                title: 'すばらしいやないか！',
+                date: '2022年2月8日 19:30',
+                address: '住之江区中加賀屋３丁目７',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49010563251517, 34.61261492920843]
+            },
+            properties: {
+                title: '東京のもんには敵わんけ',
+                date: '2022年2月9日 18:00',
+                address: '住吉区長峡町３',
+                link: 'none',
+                url: '関連記事'
+            }
+        }
+
+    ]
+};
