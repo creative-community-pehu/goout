6089d28ae3a79b9e99c4cc74a40a1ce9446c3efa
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-24 19:01:07 +0900

    v2.js

---
+++ v2.js
@@ -0,0 +1,108 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49249037915752, 34.698777739416045]
+            },
+            properties: {
+                title: 'OSAKA GARDEN CITY',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1639211015433!6m8!1m7!1sCAoSLEFGMVFpcFBPSE96OTgzaUVqREREXzZ1Rko0RU9uV1hNYzFzLVdMaDd5SU4z!2m2!1d34.6986431!2d135.4924676!3f0!4f0!5f0.7820865974627469',
+                url: 'http://vg.pe.hu/up/play/garden_city.html',
+                nlc: 'none'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49833040290451, 34.70324334937851]
+            },
+            properties: {
+                title: '阪急百貨店梅田店コンコース',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1639211054591!6m8!1m7!1sCAoSLEFGMVFpcFBaVEdndE85eUlyT0t4ZjMxWTJfbjhvZDgtekpTbERGM3R6dVJM!2m2!1d34.7028186!2d135.4985323!3f0!4f0!5f0.7820865974627469',
+                url: 'https://newlifecollection.com/ca98/1206/p-r-s/',
+                nlc: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47595609248816, 34.632416979359874]
+            },
+            properties: {
+                title: '千本松渡船場',
+                iframe: 'https://www.youtube.com/embed/SNw7eE2K0c0',
+                url: 'https://newlifecollection.com/ca98/207/p-r-s/',
+                nlc: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46306770448456, 34.6261385710099]
+            },
+            properties: {
+                title: '木津川渡船場',
+                iframe: 'https://www.youtube.com/embed/JDQjc70a1T8',
+                url: 'https://newlifecollection.com/ca98/208/p-r-s/',
+                nlc: 'none'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.45799310807644, 34.63296956355602]
+            },
+            properties: {
+                title: '船町渡船場',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1650793919941!6m8!1m7!1sCAoSLEFGMVFpcE8wWDlRdDZxVlp6Ty12b1hmc1JHaF9kNElNMjR6c0MxOTByZm00!2m2!1d34.632505!2d135.4578235!3f197.6178133032763!4f-0.872203703167159!5f0.7820865974627469',
+                url: 'https://newlifecollection.com/ca98/285/p-r-s/',
+                nlc: 'none'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.44190326912172, 34.64974551271986]
+            },
+            properties: {
+                title: '第1・第2突堤北臨港緑地',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1650793737165!6m8!1m7!1sCAoSLEFGMVFpcE1hOHJyQjBWcEdfaWxWdDJkcnA4ekNmc3JBWk5maHlNSGNVdS00!2m2!1d34.6498582!2d135.4418884!3f0!4f0!5f0.7820865974627469',
+                url: '',
+                nlc: 'none'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49361351217595, 34.69913764968524]
+            },
+            properties: {
+                title: 'HERBIS PLAZA',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1650793997909!6m8!1m7!1sCAoSLEFGMVFpcE1FOEJRSVVfYkxmVjZLNlhoYjUxcnFydnBPSEdZWjlGc1l0bVlE!2m2!1d34.6984253!2d135.4926758!3f185.84761762785757!4f-3.125840868703662!5f0.7820865974627469',
+                url: '',
+                nlc: 'none'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.44978338008536, 34.64437771141368]
+            },
+            properties: {
+                title: 'なみはや大橋',
+                iframe: 'https://www.google.com/maps/embed?pb=!4v1650794188800!6m8!1m7!1sCAoSLEFGMVFpcFBCWkhCTGhKYlltbjMxaWxtTEFFbDdHdlRQTEVfMExTcXBFWmtl!2m2!1d34.6457169!2d135.4500484!3f0!4f0!5f0.7820865974627469',
+                url: '',
+                nlc: 'none'
+            }
+        }
+    ]
+};
