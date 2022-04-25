1345527573e492025ba7c67ac01a7647619e45c5
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-25 23:42:12 +0900

    map_v15

--- otobuilding/aa_v7.js
+++ otobuilding/aa_v7.js
@@ -265,6 +265,62 @@ const geojson = {
                 link: 'none',
                 url: ''
             }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48590991829985, 34.61701733866016]
+            },
+            properties: {
+                title: 'ヘイくっさマ〜ン',
+                date: '2022年3月16日 21:16',
+                address: '住之江区粉浜西３丁目１−６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48638856608255, 34.615822882690466]
+            },
+            properties: {
+                title: 'ここで一つ、面白い話をします。',
+                date: '2022年3月19日 14:30',
+                address: '住之江区粉浜西３丁目１−５',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47776778334372, 34.620788703512616]
+            },
+            properties: {
+                title: '雨最高〜　雨やっば　雨さいっこう！',
+                date: '2022年3月19日 16:00',
+                address: '住之江区西加賀屋１−１−４１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49133472122134, 34.62641776667715]
+            },
+            properties: {
+                title: 'keep your emotion',
+                date: '2022年3月26日 14:30',
+                address: '西成区玉出中1-12-13',
+                link: 'none',
+                url: ''
+            }
         }
 
     ]
