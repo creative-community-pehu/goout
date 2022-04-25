b69cd6a8d09ffb9779dc674eb9aa30176ba74cf5
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-25 22:43:50 +0900

    aav4

---
+++ otobuilding/aa_v4.js
@@ -0,0 +1,145 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48934698606456, 34.614762993711466]
+            },
+            properties: {
+                title: 'あかんあかん、あかんてだから〜',
+                date: '2022年2月13日 19:30',
+                address: '住之江区粉浜３丁目10-18',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48806306208354, 34.61672899645829]
+            },
+            properties: {
+                title: 'たえちゃんが背中を押してくれたから今日は耐えれた',
+                date: '2022年2月16日 20:20',
+                address: '住之江区粉浜西２丁目１９',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48573910200872, 34.61480869515299]
+            },
+            properties: {
+                title: 'お風呂掃除したら50円な？',
+                date: '2022年2月23日 10:15',
+                address: '住之江区粉浜西３丁目４',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49139840617704, 34.61760551293973]
+            },
+            properties: {
+                title: '冬もの、売っちゃいます！',
+                date: '2022年2月24日 20:35',
+                address: '住吉区東粉浜３丁目２３−２３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4796960670957, 34.614665866352524]
+            },
+            properties: {
+                title: '走れクソガキ',
+                date: '2022年2月26日 19:10',
+                address: '住之江区中加賀屋４丁目６−２８',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48994910185849, 34.60640581645879]
+            },
+            properties: {
+                title: 'SHE’S 住吉',
+                date: '2022年2月27日 22:30',
+                address: '住之江区安立２丁目２',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48683721540584, 34.61318000271726]
+            },
+            properties: {
+                title: 'りおちゃんはどうしてそんなに可愛いの？',
+                date: '2022年3月1日 19:40',
+                address: '住之江区浜口西１丁目１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [座標情報]
+            },
+            properties: {
+                title: '聞いた言葉',
+                date: '日付',
+                address: '住所',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47666421864864, 34.60006459036512]
+            },
+            properties: {
+                title: '大和川ヘルスストリート',
+                date: '2022年3月5日 16:00',
+                address: '住之江区北島３丁目１６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4686466499881, 34.61044215073433]
+            },
+            properties: {
+                title: '極楽っちゅうもんや',
+                date: '2022年3月5日 16:50',
+                address: '住之江区泉１丁目１−８２',
+                link: 'none',
+                url: ''
+            }
+        }
+
+    ]
+};
