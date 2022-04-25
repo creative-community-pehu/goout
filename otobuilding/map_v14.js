092deb12c2da29a9215c0cddc924413501852f4a
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-25 22:54:49 +0900

    v14

--- otobuilding/map_v14.js
+++ otobuilding/map_v14.js
@@ -601,6 +601,132 @@ const geojson = {
                 link: '',
                 url: 'https://newlifecollection.com/ca100/1286/p-r-s/'
             }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47290133878147, 34.60749330611684]
+            },
+            properties: {
+                title: '偉かったね、もう終わったよ。よくがんばったね〜',
+                date: '2022年3月17日 16:50',
+                address: '住之江区新北島１丁目２−１',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48930246358145, 34.61194391675484]
+            },
+            properties: {
+                title: 'ドキドキするけど大丈夫！',
+                date: '2022年2月8日 15:30',
+                address: '住之江区浜口東１丁目１',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4827294191162, 34.60191716499787]
+            },
+            properties: {
+                title: 'なぁ、ほんまのこと言うて？',
+                date: '2022年2月8日 16:25',
+                address: '住之江区西住之江３丁目３',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46463988902042, 34.6063333988492]
+            },
+            properties: {
+                title: 'でっか！なんやねんこの白菜',
+                date: '2022年2月8日 18:50',
+                address: '住之江区新北島６丁目１−５５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4808284868539, 34.61827594571754]
+            },
+            properties: {
+                title: 'すばらしいやないか！',
+                date: '2022年2月8日 19:30',
+                address: '住之江区中加賀屋３丁目７',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49010563251517, 34.61261492920843]
+            },
+            properties: {
+                title: '東京のもんには敵わんけ',
+                date: '2022年2月9日 18:00',
+                address: '住吉区長峡町３',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49187281851079, 34.618000679283696]
+            },
+            properties: {
+                title: '中途半端なことがいちばん困んねやぁ',
+                date: '2022年2月13日 19:15',
+                address: '住吉区東粉浜３丁目２３',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49227968288875, 34.605819819360846]
+            },
+            properties: {
+                title: '私、初恋時代は絶対忘れへん！一生！',
+                date: '2022年2月27日 21:30',
+                address: '住吉区清水丘１丁目３１−１５',
+                link: 'none',
+                url: '関連記事'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49603212288486, 34.64744739591383]
+            },
+            properties: {
+                title: 'くっせー服',
+                date: '2022年3月26日 16:10',
+                address: '西成区鶴見橋１丁目１７−１５',
+                link: 'none',
+                url: '関連記事'
+            }
         }
 
     ]
