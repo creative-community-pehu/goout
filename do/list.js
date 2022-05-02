0ae910161151d30d9f44d6014d95dff3bc99789d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-05-02 23:32:41 +0900

    do

---
+++ do/list.js
@@ -0,0 +1,89 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48641471397286, 34.63357632592684]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:10',
+                address: '西成区千本中２丁目３−２０',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48766121074425, 34.63457791958582]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:20',
+                address: '西成区千本北２丁目２９−１８',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48680727044314, 34.635939323277086]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:22',
+                address: '西成区千本北２丁目２０−２３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.488939135542, 34.635929166387655]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:28',
+                address: '西成区千本北２丁目１８−２６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4890337713029, 34.63586664663737]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:30',
+                address: '西成区千本北１丁目１１−３６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48742963955186, 34.638752909055384]
+            },
+            properties: {
+                title: 'やるぞ ‼',
+                date: '2022年5月2日 15:40',
+                address: '西成区橘３丁目１７−６',
+                link: 'none',
+                url: ''
+            }
+        }
+
+    ]
+};
