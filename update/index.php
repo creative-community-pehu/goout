0534f5071c4cead2c142da81789718d2ce97fcf4
∧° ┐ <we.are.pe.hu@gmail.com>
2022-05-20 21:56:47 +0900

    map

--- update/index.php
+++ update/index.php
@@ -7,7 +7,7 @@ $status = (string)filter_input(INPUT_POST, 'status');
 $more = (string)filter_input(INPUT_POST, 'more');
 $link = (string)filter_input(INPUT_POST, 'link');
 
-$fp = fopen('index.csv', 'a+b');
+$fp = fopen('mapbox.csv', 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
     fputcsv($fp, [$title, $status, $more, $link]);
@@ -32,13 +32,49 @@ fclose($fp);
 <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
 <meta name="description" content="行ったことのない場所へ行く">
 <link rel="icon" href="/map/logo.png">
+<link rel="stylesheet" href="/map/css/top.css" />
+<link rel="stylesheet" href="/map/css/info.css" />
+<link rel="stylesheet" href="/map/css/fonts.css" />
 <link rel="stylesheet" href="/map/css/update.css"/>
+<link rel="stylesheet" href="https://creative-community.space/coding/fontbook/css/font-family.css" />
 <style>
-body { margin: 0; padding: 0; }
+    body {margin: 0; padding: 0; background-color:lemonchiffon;}
+    #main {width:75%; margin:2vw auto;}
+.cc {
+    font-family: "ipag", monospace;
+    transform:scale(1, 1.25);
+}
+#top_btn {
+    position: fixed;
+    bottom:0; left:0;
+    z-index: 1000;
+    margin:2.5vw;
+    font-size:2.5vw;
+}
+#top_btn a {
+    display: block;
+    text-align: center;
+    width: 5.5vw;
+    height: 4.5vw;
+    line-height: 4.5vw;
+    color: #000;
+    cursor: pointer;
+    text-decoration: none;
+    transition: all 1000ms ease;
+}
+#top_btn a:hover {
+    color: lightskyblue;
+    cursor: pointer;
+    transition: all 1000ms ease;
+}
+    
 </style>
 </head>
 
 <body>
+<p id="top_btn"><a class="cc" href="#" onclick="history.back(-1);return false;">↵</a></p>
+
+<div id="main">
 <ul id="update_log">
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
@@ -46,7 +82,7 @@ body { margin: 0; padding: 0; }
 <h2><u><?=h($row[0])?></u><br/>
 <i><?=h($row[1])?></i></h2>
 <p><b><?=h($row[2])?></b>
-<a href="<?=h($row[3])?>" target="_parent"></a></p>
+<a href="<?=h($row[3])?>" target="_blank" rel=”noopener noreferrer”></a></p>
 </li>
 <?php endforeach; ?>
 <?php else: ?>
@@ -54,9 +90,15 @@ body { margin: 0; padding: 0; }
 <h2><u>タイトル</u><br/>
 <i>進行状況</i></h2>
 <p><b>説明</b>
-<a href="<?=h($row[3])?>" target="_parent"></a></p>
+<a href="<?=h($row[3])?>" target="_blank" rel=”noopener noreferrer”></a></p>
 </li>
 <?php endif; ?>
+<li>
+<h2 class="relax">地図を作ろう<br/>
+<i class="goout">Digital Map</i></h2>
+<p class="relax"><b>Mapboxを使って、初めて訪れた場所・お気に入りの場所を集めたデジタル地図を作成します。</b></p>
+</li>
 </ul>
+</div>
 </body>
 </html>
